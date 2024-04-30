3ee461a42f54d99e513194504137c79c2857c7ea
buff v <alexandervanderleek@gmail.com>
2024-04-30 12:37:34 +0200

    s3 bucket uploading working

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -14,7 +14,7 @@ export async function POST(req:Request){
         const {name, type} =  file;
         const data = await file.arrayBuffer();
         
-        const client = new S3Client({
+        const s3client = new S3Client({
             region: "af-south-1",
             credentials: {
                 accessKeyId: process.env.AWS_ACCESS_KEY||"" ,
@@ -23,17 +23,24 @@ export async function POST(req:Request){
         });
 
         const id = uniqid()
-        const ext = name.split('.')[-1]
+        const ext = name.split('.').slice(-1)[0]
         const newName = id + "." + ext
 
-        // const uploadCommand = new PutObjectCommand({
-        //     Bucket: process.env.BUCKET_NAME,
-        //     Body: data,
-        //     ACL: 'public-read',
-        //     ContentType: type,
-        //     Key: name
-        // })
-        return Response.json({name, ext, newName});
+
+        const uploadCommand = new PutObjectCommand({
+            Bucket: process.env.BUCKET_NAME,
+            Body: data,
+            ACL: 'public-read',
+            ContentType: type,
+            Key: newName
+        })
+
+
+        await s3client.send(uploadCommand);
+
+
+
+        return Response.json({name, ext, newName, id});
     }
 
     
--- app/components/UploadForm.tsx
+++ app/components/UploadForm.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import UploadIcon from './uploadIcon'
 import { ChangeEvent } from "react";
 import axios from 'axios';
@@ -7,18 +7,21 @@ import axios from 'axios';
 
 const UploadForm = () => {
 
+    const [uploading, setIsUploading] = useState<Boolean>(false);
+
 async function upload(e:ChangeEvent<HTMLInputElement>){
-    console.log("changing things")
     e.preventDefault();
-    console.log(e);
 
     const files = e.target.files;
 
     if(files && files.length>0){
         const file = files[0];
+
+        setIsUploading(true);
         const res = await axios.postForm('/api/upload',{
             file:file
         })
+        setIsUploading(false);
 
         console.log(res.data)
     }
@@ -26,11 +29,23 @@ async function upload(e:ChangeEvent<HTMLInputElement>){
     }
 
   return (
-    <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 hover:cursor-pointer">
-        <UploadIcon />
-        <span>Choose file</span>
-        <input onChange={(e)=>upload(e)} type="file" className="hidden" />
-    </label>
+    <>
+        {uploading && (
+            <div className='bg-black/85 text-white fixed inset-0 flex items-center'>
+                <div className='w-full text-center font-bold'>
+                    <h2 className='text-4xl mb-4'>Uploading</h2>
+                    <h3 className='text-xl'>Please Wait</h3>
+                </div>
+                
+            </div>
+        )}
+        <label className="bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50 hover:cursor-pointer">
+            <UploadIcon />
+            <span>Choose file</span>
+            <input onChange={(e)=>upload(e)} type="file" className="hidden" />
+        </label>
+    </>
+    
   )
 }
 

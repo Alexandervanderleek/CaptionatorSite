472b4d59d0ddf6585d616dc21ccee6b976e9a370
buff v <alexandervanderleek@gmail.com>
2024-05-01 18:59:23 +0200

    transcription being returned

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -1,8 +1,12 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import {
   GetTranscriptionJobCommand,
   StartTranscriptionJobCommand,
   TranscribeClient,
 } from "@aws-sdk/client-transcribe";
+import { Stream } from "stream";
+
+
 
 function getClient() {
   return new TranscribeClient({
@@ -12,6 +16,50 @@ function getClient() {
       secretAccessKey: process.env.AWS_SECRET_KEY || "",
     },
   });
+}
+
+async function streamToString(stream: Stream){
+  
+  const chunks: Uint8Array[] | Buffer[] = [];
+  
+  return new Promise((resolve, reject)=>{
+    stream.on("data", (chunk)=>
+      chunks.push(Buffer.from(chunk))
+    )
+    stream.on("end",() => resolve(Buffer.concat(chunks).toString("utf-8")))
+    stream.on("error", reject)
+  })
+}
+
+async function getTranscriptionfile(filename:string) {
+  const transcriptionFileName = filename + ".transcription";
+
+  const s3client = new S3Client({
+    region: "af-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY||"" ,
+        secretAccessKey: process.env.AWS_SECRET_KEY||"",
+    },
+  });
+
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: process.env.BUCKET_NAME,
+    Key: transcriptionFileName
+  });
+
+  let transcribeRes = null
+  try{
+    transcribeRes = await s3client.send(getObjectCommand)
+  }catch(e){
+
+  }
+
+  if(transcribeRes?.Body){
+    const result  =  await streamToString(transcribeRes.Body as Stream);
+    console.log(result);
+   
+  }
+
 }
 
 function createTranscriptionCommand(filename: string) {
@@ -52,6 +100,12 @@ export async function GET(request: Request) {
   const url = new URL(request.url);
   const searchParams = new URLSearchParams(url.searchParams);
   const filename = searchParams.get("filename");
+  
+
+
+  //find ready transription
+
+  await getTranscriptionfile(filename||"")
 
   //check if already have job
 
